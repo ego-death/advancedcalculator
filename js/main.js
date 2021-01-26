5cebeb84063b660011d4c9aee310cad9640142bf
Sidharth Soman <siddu.soman65@gmail.com>
2021-01-27 02:10:23 +0530

    final commit for day 1. probably will finish tomorrow. got calculator working for addition but have to figure out functionality for interlacing equals and operator buttons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,52 @@
+//jesus dont want me for a sunbeam :(
+//TODO: 26/01/2020 - COMPLETE OPERATION FUNCTION!!!
+//TODO: 27/01/2020 - SOMEHOW GET DECIMALS WORKING!!!!!!
+
+let a; //fucking keep it wtf
+let b;
+let operation;
+let total=0;
+let flag=false;
+const display=document.querySelector('#insertNum');
+
+const numBtns=document.getElementsByClassName('num');
+let numBtnsArr = Array.prototype.slice.call(numBtns).map(element=>element.addEventListener('click', function(e){
+	if(flag){display.innerText="";flag=false;}
+	console.log(e.target);
+	display.innerText+=e.target.innerText;
+}));
+
+const opBtns=document.getElementsByClassName('operation');
+let opBtnsArr = Array.prototype.slice.call(opBtns).map(element=>element.addEventListener('click', function(e){
+	console.log(e.target);
+	operation=e.target.innerText;
+	if(!a){
+		a=parseInt(display.innerText);
+		total+=a;
+	}
+	else{
+		b=parseInt(display.innerText);
+		switch(operation){
+			case '+': total=a+b;
+					break;
+			case '-': total=a-b;
+					break;
+			case 'x': total=a*b;
+					break;
+			case '/': total=a/b;
+					break;
+		}
+	}
+	console.log(e.target.innerText, total);
+	display.innerText="";
+}));
+
+document.querySelector('#equals').addEventListener('click', function(){
+	let keepThis=(parseInt(display.innerText));
+	display.innerText=(total+=keepThis);
+	flag=true;
+});
+
 
 
 
